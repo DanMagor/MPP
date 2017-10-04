aacc679c20451cdd1eb9eb8df22b17766d1aa6e8
Anton Skudarnov <Anton Skudarnov>
2017-10-05 01:43:48 +0300

    Comments added

--- Assignment 7/Program.cs
+++ Assignment 7/Program.cs
@@ -7,33 +7,51 @@ namespace Assignment6
     {
         public static void Main(string[] args)
         {
-            var year = new Year("BS2");
+            //Basic instances initialization.
+            var year = new Year("BS2");               
             var name = new Name("Ivan", "Tkachenko");
             var instructorName = new Name("Evgeniy", "Zuev");
             var assistantName = new Name("Evgeniy", "Bobrov");
             var instructor = new Instructor(instructorName);
             var assistant = new Assistant(assistantName);
+          
+            //Create array of Assistants for course.
             Assistant[] mppAssistants = {assistant};
+            
+            //Create course.
             var lectureCourse = new LectureCourse("MPP", year, instructor, mppAssistants, true);
+            
+            //Array of courses.
             LectureCourse[] lectureCourses = {lectureCourse};
+            
+            //Some additional basic instanceses.
             var student = new Student(name, year, lectureCourses);
             var auditorium = new Auditorium("Fri", "09:00", 106);
             var lecture = new Lecture(lectureCourse, auditorium);
+            
+            //Array of lectures
             Lecture[] lectures = {lecture};
+            
+            //Create a day and fill week with the same day, just for demostration. 
             var exampleDay = new TeachingDay(lectures);
             var teachingWeek = new TeachingWeek(exampleDay, exampleDay, exampleDay, exampleDay, exampleDay, exampleDay,
                 exampleDay);
+            
+            //Array of weeks with one week for schedule.
             TeachingWeek[] teachingWeeks = {teachingWeek};
             var teachingSchedule = new TeachingSchedule(teachingWeeks);
 
-
+            //Create arrays with one element for university object.
             Student[] students = {student};
             Instructor[] instructors = {instructor};
             Assistant[] assistants = mppAssistants;
 
+            //Create University
             var innopolisUniversity =
                 new University(students, assistants, instructors, lectureCourses, teachingSchedule);
 
+            
+            //Some output demonstration, set operation works as well.
             Console.Write(innopolisUniversity.Students[0].Name.GivenName + " " +
                           innopolisUniversity.Students[0].Name.FamilyName);
             Console.Write(" Year:" + innopolisUniversity.Students[0].Year.Value + " ");
